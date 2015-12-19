1198065008774c594b1c7990c94c18828543101c
Ivan Srdić <ivansrdic93@gmail.com>
2015-12-19 10:50:07 +0100

    Added Twitter authentication for simultaneous users

--- twitter_stream_server/src/main/java/Main.java
+++ twitter_stream_server/src/main/java/Main.java
@@ -1,20 +1,62 @@
+import com.github.scribejava.apis.TwitterApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.*;
+import com.github.scribejava.core.oauth.OAuthService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 /**
  * Created by Ivan on 16.12.2015..
  */
 public class Main {
+    public static Map<String, Token> accessTokens;
     public static void main (String[] args) {
-        TwitterHBC twitter = new TwitterHBC("St66y6VZapKDx5j08cfRTn6JA", "wdQNykEmXR5vVHJpWdI4jVN5CPz0sPCjSWeuUeG9CtHpyiLAGk",
-                "4436645361-YKCs94WpK6bbPrXeKxZBRbhiwNPPWqv91KR8Rzr", "OFrccO6cqsX0P31fP2MNjhZ7s3Dr6dJrEjwAQyXVf2Sb7",
-                "Rassus 16");
-        twitter.connect();
-        while (!twitter.isDone()) {
-            String msg = null;
-            try {
-                msg = twitter.getMessage();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        accessTokens = new HashMap<>();
+        // Create an OAuth service using ScribeJava using app key and secret and a callback after authenticating on Twitter
+        OAuthService service = new ServiceBuilder()
+                .provider(TwitterApi.class)
+                .apiKey("St66y6VZapKDx5j08cfRTn6JA")
+                .apiSecret("wdQNykEmXR5vVHJpWdI4jVN5CPz0sPCjSWeuUeG9CtHpyiLAGk")
+                .callback("http://127.0.0.1:4567/confirmAuth")
+                .build();
+
+        // Initialize a web socket server
+        webSocket("/stream", StreamingWebSocket.class);
+
+        // Permit JavaScript redirection
+        before("/auth", (req, resp) -> resp.header("Access-Control-Allow-Origin", "*"));
+        // First step of authorization
+        get("/auth", (req, resp) -> {
+            // If a user has a requestToken cookie, and the requestToken is a valid key, just redirect him to the stream
+            if(req.cookie("requestToken") != null && accessTokens.containsKey(req.cookie("requestToken"))) {
+                resp.cookie("requestToken", req.cookie("requestToken"));
+                resp.redirect("http://127.0.0.1/RASSUS/rassus16/public_html/stream.html");
+                return "";
             }
-            System.out.println(msg);
-        }
+            // If he doesn't get a new request token and redirect him to Twitter to log in and authorize the app
+            else {
+                Token requestToken = service.getRequestToken();
+                req.session().attribute("requestToken", requestToken);
+                resp.redirect("https://api.twitter.com/oauth/authenticate?oauth_token="+requestToken.getToken(), 302);
+                return "";
+            }
+        });
+
+        // Second step of authorization
+        get("/confirmAuth", (req, resp) -> {
+            Token requestToken = req.session().attribute("requestToken");
+            Verifier v = new Verifier(req.queryParams("oauth_verifier"));
+            // Get the access token by using the request token and verifier (from Twitter)
+            Token accessToken = service.getAccessToken(requestToken, v);
+            // Save it for further use
+            accessTokens.put(requestToken.getToken(), accessToken);
+            // Redirect the user to the stream with requestToken cookie
+            resp.cookie("requestToken", requestToken.getToken());
+            resp.redirect("http://127.0.0.1/RASSUS/rassus16/public_html/stream.html");
+            return "";
+        });
     }
 }
---
+++ twitter_stream_server/src/main/java/StreamingWebSocket.java
@@ -0,0 +1,44 @@
+import com.github.scribejava.core.model.Token;
+import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+@WebSocket
+public class StreamingWebSocket {
+
+    // Store sessions if you want to, for example, broadcast a message to all users
+    private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
+
+    @OnWebSocketConnect
+    public void connected(Session session) {
+        sessions.add(session);
+    }
+
+    @OnWebSocketClose
+    public void closed(Session session, int statusCode, String reason) {
+        sessions.remove(session);
+    }
+
+    @OnWebSocketMessage
+    public void message(Session session, String message) throws IOException {
+        // First message is the request token
+        Token accessToken = Main.accessTokens.get(message);
+        RemoteEndpoint remote = session.getRemote();
+        TwitterHBC twitter = new TwitterHBC("St66y6VZapKDx5j08cfRTn6JA", "wdQNykEmXR5vVHJpWdI4jVN5CPz0sPCjSWeuUeG9CtHpyiLAGk",
+                                                accessToken.getToken(), accessToken.getSecret(),
+                                                "Rassus 16");
+        twitter.connect();
+        while (!twitter.isDone()) {
+            String msg = null;
+            try {
+                msg = twitter.getMessage();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            remote.sendString(msg);
+            System.out.println(msg);
+        }
+    }
+}
